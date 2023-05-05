3e3bc11d018ef518243cc384c78a181f04b4acbe
anapisarek <anabeblo@gmail.com>
2023-05-05 21:43:29 +0200

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,81 +2,118 @@ import flatpickr from 'flatpickr';
 import Notiflix from 'notiflix';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const datePickrEl = document.getElementById('datetime-picker');
-const startEl = document.querySelector('button[data-start]');
-const daysEl = document.querySelector('[data-days]');
-const hoursEl = document.querySelector('[data-hours]');
-const minutesEl = document.querySelector('[data-minutes]');
-const secondsEl = document.querySelector('[data-seconds]');
+//Adding the HTML document elements to javascript
+const dataInputEl = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const pauseButton = document.querySelector('[data-pause]');
+const resetButton = document.querySelector('[data-reset]');
+const daysLeft = document.querySelector('[data-days]');
+const hoursLeft = document.querySelector('[data-hours]');
+const minutesLeft = document.querySelector('[data-minutes]');
+const secondsLeft = document.querySelector('[data-seconds]');
 
-const workingEl = document.querySelector('[data-working]');
-const workSymbols = ['\u259A', '\u259E'];
-let currentSymbolIndex = 0;
+//Add null variable to declare and clear Intervals.
+let countDown = null;
 
-startEl.disabled = true;
+//Variables to store and update milliseconds Left and object to store those milliseconds as human friendly date
+let pickedDate = 0;
+let millisecondsLeft = 0;
+let timeLeft = {};
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    if (options.defaultDate <= selectedDates[0]) {
-      startEl.disabled = false;
-    } else {
-      Notiflix.Notify.failure('Please choose a date in the future');
-      startEl.disabled = true;
-    }
-  },
-};
+startButton.disabled = true;
+pauseButton.disabled = true;
+resetButton.disabled = true;
 
-flatpickr(datePickrEl, options);
-
-function convertMs(ms) {
+//Function that convert Milisecond from .getTime() to date in days hours minutes and seconds
+const convertMs = ms => {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
+  // Remaining days
   const days = Math.floor(ms / day);
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+};
 
+//Add 0 to the beginning of string if timenumber < 9
 const addLeadingZero = value => {
-  if (value.toString().length === 1) {
-    return value.toString().padStart(2, '0');
-  }
+  if (value < 10) return value.toString().padStart(2, '0');
   return value;
 };
 
-datePickrEl.addEventListener('input', e => {
-  const pickedDate = new Date(e.currentTarget.value).getTime();
+//Function that is going to be activated only once when the counter goes to 0
+const endCountdown = () => {
+  clearInterval(countDown);
+  daysLeft.textContent = '00';
+  hoursLeft.textContent = '00';
+  minutesLeft.textContent = '00';
+  secondsLeft.textContent = '00';
+};
 
-  startEl.addEventListener('click', () => {
-    const countdown = setInterval(() => {
-      const currentDate = new Date().getTime();
-      const timeLeft = pickedDate - currentDate;
+//Function that updates Time Left (-1 second every time) and putting it to the HTML textContent
+const updateTimeLeft = () => {
+  timeLeft = convertMs(millisecondsLeft);
+  daysLeft.textContent = addLeadingZero(timeLeft.days);
+  hoursLeft.textContent = addLeadingZero(timeLeft.hours);
+  minutesLeft.textContent = addLeadingZero(timeLeft.minutes);
+  secondsLeft.textContent = addLeadingZero(timeLeft.seconds);
+  millisecondsLeft -= 1000;
+  if (millisecondsLeft < 0) {
+    endCountdown();
+    Notiflix.Report.success('Countdown ended!', 'The time has come!', 'Thank you!');
+  }
+};
 
-      secondsEl.innerHTML = addLeadingZero(convertMs(timeLeft).seconds);
-      minutesEl.innerHTML = addLeadingZero(convertMs(timeLeft).minutes);
-      hoursEl.innerHTML = addLeadingZero(convertMs(timeLeft).hours);
-      daysEl.innerHTML = addLeadingZero(convertMs(timeLeft).days);
-      currentSymbolIndex = (currentSymbolIndex + 1) % workSymbols.length;
-      workingEl.textContent = 'Working ' + workSymbols[currentSymbolIndex];
-      startEl.disabled = true;
+//FlatPickr options that declares how the date picker should look and behave.
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  //minDate: new Date(),   <- Disabling every date in the past - User cannot chose it.
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    pickedDate = selectedDates[0].getTime();
+    millisecondsLeft = pickedDate - new Date().getTime();
+    if (millisecondsLeft < 0)
+      return Notiflix.Report.failure(
+        'Please choose a date in the future',
+        '',
+        'OK will do captain!'
+      );
+    startButton.disabled = false;
+  },
+};
 
-      if (timeLeft < 0) {
-        clearInterval(countdown);
-        secondsEl.innerHTML = addLeadingZero(0);
-        minutesEl.innerHTML = addLeadingZero(0);
-        hoursEl.innerHTML = addLeadingZero(0);
-        daysEl.innerHTML = addLeadingZero(0);
-        workingEl.innerHTML = 'Done' + '\u263A';
-      }
-    }, 1000);
-  });
+flatpickr(dataInputEl, options);
+startButton.addEventListener('click', () => {
+  millisecondsLeft = pickedDate - new Date().getTime();
+  clearInterval(countDown);
+  countDown = setInterval(() => updateTimeLeft(), 1000);
+  startButton.disabled = true;
+  pauseButton.disabled = false;
+  resetButton.disabled = false;
+});
+
+pauseButton.addEventListener('click', () => {
+  clearInterval(countDown);
+  Notiflix.Notify.warning('Countdown paused');
+  startButton.disabled = false;
+  pauseButton.disabled = true;
+});
+
+resetButton.addEventListener('click', () => {
+  pauseButton.disabled = true;
+  resetButton.disabled = true;
+  clearInterval(countDown);
+  endCountdown();
+  Notiflix.Notify.warning('Countdown reseted');
 });
